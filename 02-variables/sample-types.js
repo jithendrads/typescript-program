a1d1cec8b098516694c908672da3034b855058df
Jithendra <jithendrads@gmail.com>
2020-12-03 13:26:24 +0530

    created variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,10 @@
+var found = true;
+var grade = 90;
+var firstName = "jithendra";
+var lastName = "srinivas";
+console.log(found);
+console.log("The grade is " + grade);
+//using string concatenation
+console.log("Hi " + firstName + " " + lastName);
+//using template string
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,12 @@
+let found:boolean=true
+let grade:number=90
+let firstName:string="jithendra"
+let lastName:string="srinivas"
+
+console.log(found)
+console.log("The grade is "+ grade)
+//using string concatenation
+console.log("Hi "+ firstName + " "+ lastName)
+
+//using template string
+console.log(`Hi ${firstName} ${lastName}`)
